2beb285c4c86e5c8f22fc012897e360e0525d2d3
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-02-03 15:05:28 -0300

    arrays

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/arrays/scripts.js
@@ -0,0 +1,37 @@
+let pizzas = ['4 queijos', 'Frango com Catupiry', 'Marguerita', 'Palmito', 'Chocolate'];
+
+// pizzas.push('Peito de Peru'); // push - > insere valor na ultima posição
+// pizzas.push('Gratinada');
+// pizzas.pop(); // remove o ultimo item do array
+//pizzas.shift(); // remove o primeito item do array
+//pizzas.unshift("pizza 1"); //insere o valor no inicio do array
+
+// console.log(pizzas.length); // lenght -> retorna o numero de posições
+// let ultimoItem = pizzas[pizzas.length - 1]; // ultimo item do array
+// console.log(ultimoItem);
+
+//console.log(pizzas.sort()); // .sort() -> retonra array em ordem crescente ou a-z
+
+//console.log(pizzas[0]); // primeiro item do array
+
+
+//console.log(pizzas.indexOf("Palmito")) // indexof() - procurar o indice no array
+
+//exibir array com for
+
+for (let index = 0; index < pizzas.length; index++) {
+    console.log(pizzas[index]);
+}
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+menu.push('Contato')
+
+console.log(menu);
