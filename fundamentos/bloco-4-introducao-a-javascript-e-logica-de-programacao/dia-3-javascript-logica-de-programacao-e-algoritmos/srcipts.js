c0a17716fdcc0fc4e041f5c8584b5f27866e91c2
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-02-04 15:12:36 -0300

    scripts.js dia3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/srcipts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/srcipts.js
@@ -1,4 +1,4 @@
-let fruits = [3, 4, 10, 1, 12];
+/* let fruits = [3, 4, 10, 1, 12];
 let sum = 0;
 
 for (let index = 0; index < fruits.length; index += 1) {
@@ -10,4 +10,23 @@ if (sum > 15) {
     console.log(sum);
 } else {
     console.log("valor menor que 16");
+} */
+
+/* const n = 9;
+
+let resultado = 100;
+
+for (let i = 0; i <= n; i += 1) {
+    resultado -= i;
+    console.log(resultado);
 }
+console.log(resultado); */
+
+const n = 3;
+
+let resultado = 0;
+
+for (let i = 0; i <= n; i += 1) {
+    resultado += i
+}
+console.log(resultado);
