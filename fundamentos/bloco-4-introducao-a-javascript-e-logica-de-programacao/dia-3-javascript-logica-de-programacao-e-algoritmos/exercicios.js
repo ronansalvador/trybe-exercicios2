237ef006311af786312110b198fa3c2285d81c62
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-02-04 16:23:13 -0300

    exercicios.js

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -0,0 +1,46 @@
+// Exericio 1 - Fatorial
+
+let num = 4;
+let fatorial = 1; // inicia em 1
+
+for (let index = 1; index <= num; index += 1) {
+    fatorial *= index;
+}
+
+console.log(fatorial);
+
+// Exercicio 2 - inverter palavra
+
+let word = 'tryber';
+let invertida = '';
+
+/* for (let index = 0; index < word.length; index++) {
+    console.log(word[index]);
+
+} */
+
+for (let index = word.length - 1; index >= 0; index--) {
+    invertida += word[index];
+}
+
+console.log(invertida);
+
+// exercicio 3 - maior palavra do array e menor palavra considerando o numero de caractere de cada palavra
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let palavra = 0;
+let maiorpalavra = 0;
+let texto = '';
+
+for (let index = 0; index < array.length; index += 1) {
+
+    palavra = (array[index].length);
+
+    if (palavra > maiorpalavra) {
+        maiorpalavra = palavra;
+        texto = array[index];
+    }
+
+}
+
+console.log("a maior palavra é " + texto + " com " + maiorpalavra + " caracteres");
