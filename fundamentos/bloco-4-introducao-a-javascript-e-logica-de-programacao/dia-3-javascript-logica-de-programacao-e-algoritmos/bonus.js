f5e0d9dc03321c7e49641edf4610ba44a1606b29
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-02-07 10:39:46 -0300

    bonus

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
@@ -0,0 +1,12 @@
+let numero = 3;
+let array = [];
+
+if (numero > 1) {
+
+    for (let index = 0; index < numero; index += 1) {
+        array.push("*");
+        console.log(array[index]);
+
+    }
+
+}
