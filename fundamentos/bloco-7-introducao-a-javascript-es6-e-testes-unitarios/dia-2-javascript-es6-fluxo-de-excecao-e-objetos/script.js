feb21a1edbdd0324b164d6f92aa4fe9c5ba835e5
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-03-06 11:27:35 -0300

    7.2

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -0,0 +1,34 @@
+/* const sum = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    return 'Os valores devem ser numéricos';
+  }
+  return value1 + value2;
+};
+
+console.log(sum(2, '3'));
+ */
+/* const sum = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+  return value1 + value2;
+};
+
+console.log(sum(2, '3')); */
+
+const verifyIsNumber = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+};
+
+const sum = (value1, value2) => {
+  try {
+    verifyIsNumber(value1, value2);
+    return value1 + value2;
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+console.log(sum(2, '3'));
---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/test.js
@@ -0,0 +1,12 @@
+const band = {
+  name: 'Blind Guardian',
+  formedIn: 1986,
+  lastAlbum: 'Beyond the Red Mirror',
+};
+
+const info = {
+  genre: 'Power Metal',
+  lastAlbum: 'Twilight Orchestra: Legacy of the Dark Lands',
+};
+
+console.log(Object.assign(band, info));
