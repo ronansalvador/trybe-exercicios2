5eb7911b7ee9baded4f5a1c8fd91f40f85ed1367
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-03-11 12:44:12 -0300

    dia 8.2

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus.js
@@ -0,0 +1,150 @@
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+// 1 - Crie uma função que retorna o dano do dragão.
+
+const dragonAttack = (dragon) => {
+  const attackMin = 15;
+  const dragonDmg = Math.floor((Math.random() * (dragon. strength - attackMin + 1) + attackMin));
+  return dragonDmg;
+};  
+
+console.log(dragonAttack(dragon));
+
+// 2 Crie uma função que reotorna o dano causado pelo warrior .
+
+const warriorAttack = (warrior) => {
+  const minDmg = warrior.strength;
+  const maxDmg = minDmg * warrior.weaponDmg;
+  const warriorDamage = Math.floor((Math.random() * (maxDmg - minDmg + 1)) + minDmg);
+  return warriorDamage;
+};
+
+// 3 - Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+
+const mageAttack = (mage) => {
+  const mageMana = mage.mana;
+  const minDmg = mage.intelligence;
+  const maxDmg = minDmg * 2;
+  const turnStats = {
+    manaSpent: 0,
+    damageDealt: 'Not enough mana...',
+  };
+
+  if (mageMana > 15) {
+    const mageDamage = Math.floor((Math.random() * (maxDmg - minDmg + 1)) + minDmg);
+    turnStats.manaSpent = 15;
+    turnStats.damageDealt = mageDamage;
+    return turnStats;
+  }
+  return turnStats;
+};
+
+
+// Parte II
+
+//1 
+const gameActions = {
+  warriorTurn: (warriorAttack) => {
+    const warriorDamage = warriorAttack(warrior);
+    warrior.damage = warriorDamage;
+    dragon.healthPoints -= warriorDamage;
+  },
+};
+
+gameActions.warriorTurn(warriorAttack);
+
+//2
+
+const gameActions = {
+  warriorTurn: (warriorAttack) => {
+    const warriorDamage = warriorAttack(warrior);
+    warrior.damage = warriorDamage;
+    dragon.healthPoints -= warriorDamage;
+  },
+  mageTurn: (mageAttack) => {
+    const mageTurnStats = mageAttack(mage);
+    const mageDamage = mageTurnStats.damageDealt;
+    mage.damage = mageDamage;
+    mage.mana -= mageTurnStats.manaSpent;
+    dragon.healthPoints -= mageDamage;
+  },
+};
+
+gameActions.warriorTurn(warriorAttack);
+gameActions.mageTurn(mageAttack);
+
+//3
+
+const gameActions = {
+  warriorTurn: (warriorAttack) => {
+    const warriorDamage = warriorAttack(warrior);
+    warrior.damage = warriorDamage;
+    dragon.healthPoints -= warriorDamage;
+  },
+  mageTurn: (mageAttack) => {
+    const mageTurnStats = mageAttack(mage);
+    const mageDamage = mageTurnStats.damageDealt;
+    mage.damage = mageDamage;
+    mage.mana -= mageTurnStats.manaSpent;
+    dragon.healthPoints -= mageDamage;
+  },
+  dragonTurn: (dragonAttack) => {
+    const dragonDamage = dragonAttack(dragon);
+    mage.healthPoints -= dragonDamage;
+    warrior.healthPoints -= dragonDamage;
+    dragon.damage = dragonDamage;
+  },
+};
+
+gameActions.warriorTurn(warriorAttack);
+gameActions.mageTurn(mageAttack);
+gameActions.dragonTurn(dragonAttack);
+
+// 4
+
+const gameActions = {
+  warriorTurn: (warriorAttack) => {
+    const warriorDamage = warriorAttack(warrior);
+    warrior.damage = warriorDamage;
+    dragon.healthPoints -= warriorDamage;
+  },
+  mageTurn: (mageAttack) => {
+    const mageTurnStats = mageAttack(mage);
+    const mageDamage = mageTurnStats.damageDealt;
+    mage.damage = mageDamage;
+    mage.mana -= mageTurnStats.manaSpent;
+    dragon.healthPoints -= mageDamage;
+  },
+  dragonTurn: (dragonAttack) => {
+    const dragonDamage = dragonAttack(dragon);
+    mage.healthPoints -= dragonDamage;
+    warrior.healthPoints -= dragonDamage;
+    dragon.damage = dragonDamage;
+  },
+  turnResults: () => battleMembers,
+};
+
+gameActions.warriorTurn(warriorAttack);
+gameActions.mageTurn(mageAttack);
+gameActions.dragonTurn(dragonAttack);
+console.log(gameActions.turnResults());
