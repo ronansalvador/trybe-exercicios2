9dd08d3a600e20afe171b2da5845761875c79b83
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-04-04 14:29:56 -0300

    iniciando o modulo front-end

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/hello-world/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './Component';
+
+function App() {
+  return <HelloWorld />;
+}
+
+export default App;
